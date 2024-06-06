a98d56e25e065f3dbf489a85e8895d6324425cc4
AliTahaa <ali.mohamed.taha.h@gmail.com>
2024-06-07 02:54:21 +0300

    hmm 10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,5 +8,7 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
+	char i;
+	i = (tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0;
+	return (i);
 }
