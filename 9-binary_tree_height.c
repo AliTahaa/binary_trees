298b1e65b93823bde15610c628a19726024b5a4c
AliTahaa <ali.mohamed.taha.h@gmail.com>
2024-06-07 02:51:51 +0300

    9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,6 +10,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left = 0;
 	size_t right = 0;
+	size_t height = 0;
 
 	if (tree == NULL)
 	{
@@ -22,6 +23,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 			left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 			right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 		}
-		return ((left > right) ? left : right);
+		height = (left > right) ? left : right;
+		return (height);
 	}
 }
